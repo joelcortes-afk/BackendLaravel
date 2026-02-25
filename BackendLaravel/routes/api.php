b1cc192742103989d435f5b32debaa5d098c3e13
joel cortes <joel.cortes@serpomar.com>
2026-02-25 09:18:20 -0500

    feat: primer end point con controlador

--- BackendLaravel/routes/api.php
+++ BackendLaravel/routes/api.php
@@ -1,6 +1,12 @@
 <?php
+
+namespace App\Http\Controllers;
+
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BackendController;
 
 Route::get('/test', function () {
     return "El backend funciona correctamente.";
 });
+
+Route::get('/backend', [BackendController::class, 'get']);
