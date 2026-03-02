41fa8ddb2c2acf7bf9adac47525b0d62ab105d05
joel cortes <joel.cortes@serpomar.com>
2026-03-02 15:23:54 -0500

    fix : dirrecion archivos controller

---
+++ BackendLaravel/app/Http/Controllers/Controllers/BackendController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class BackendController extends Controller
+{
+    public function get()
+    {
+        return response()->json([
+            'success' => true,
+            'message' => 'El backend funciona correctamente.'
+        ]);
+    }
+}
